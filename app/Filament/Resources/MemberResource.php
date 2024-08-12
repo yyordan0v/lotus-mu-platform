f77d993e27c8271c8f38b444f29805212cca42c3
Yordan Yordanov <yoyordanov94@gmail.com>
2024-08-12 15:50:11 +0300

    wip

--- app/Filament/Resources/MemberResource.php
+++ app/Filament/Resources/MemberResource.php
@@ -7,6 +7,7 @@ use App\Filament\Resources\MemberResource\Pages;
 use App\Models\Member;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Support\Colors\Color;
 use Filament\Tables;
 use Filament\Tables\Table;
 
@@ -17,6 +18,11 @@ class MemberResource extends Resource
     protected static ?string $navigationGroup = 'Account & Characters';
 
     protected static ?int $navigationSort = 2;
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 
     public static function form(Form $form): Form
     {
@@ -40,11 +46,25 @@ class MemberResource extends Resource
                     ->searchable(),
                 Tables\Columns\TextColumn::make('AccountLevel')
                     ->label('Account Level')
+                    ->icon(function ($state, $record) {
+                        return $record->AccountLevel === AccountLevel::Regular ? '' : 'heroicon-o-fire';
+                    })
+                    ->iconColor(function ($state, $record) {
+                        return match ($record->AccountLevel) {
+                            AccountLevel::Bronze => Color::Orange,
+                            AccountLevel::Silver => Color::Gray,
+                            AccountLevel::Gold => Color::Yellow,
+                            default => '',
+                        };
+                    })
                     ->sortable(),
                 Tables\Columns\TextColumn::make('AccountExpireDate')
-                    ->label('Account Expire Date')
+                    ->label('VIP Expire Date')
                     ->dateTime()
-                    ->sortable(),
+                    ->sortable()
+                    ->formatStateUsing(function ($state, $record) {
+                        return $record->AccountLevel === AccountLevel::Regular ? '-' : $state;
+                    }),
             ])
             ->filters([
                 Tables\Filters\SelectFilter::make('AccountLevel')

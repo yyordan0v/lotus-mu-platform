c343b5c4a5ea128b11289be2f03ad8747f33f73a
Yordan Yordanov <yoyordanov94@gmail.com>
2024-09-23 11:35:56 +0300

    install laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
 use App\Livewire\UpcomingEvents;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
 Route::get('/upcoming-events', UpcomingEvents::class)->name('upcoming-events');
+
+require __DIR__.'/auth.php';

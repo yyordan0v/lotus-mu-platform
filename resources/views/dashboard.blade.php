97435b39e51983c4ebdaa8e825158f2b4cb7dc94
Yordan Yordanov <yoyordanov94@gmail.com>
2024-09-23 23:52:22 +0300

    install flux

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,25 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <flux:checkbox.group label="Subscription preferences">
+                        <flux:checkbox checked
+                                       value="newsletter"
+                                       label="Newsletter"
+                                       description="Receive our monthly newsletter with the latest updates and offers."
+                        />
+                        <flux:checkbox
+                            value="updates"
+                            label="Product updates"
+                            description="Stay informed about new features and product updates."
+                        />
+                        <flux:checkbox
+                            value="invitations"
+                            label="Event invitations"
+                            description="Get invitations to our exclusive events and webinars."
+                        />
+                    </flux:checkbox.group>
                 </div>
             </div>
         </div>

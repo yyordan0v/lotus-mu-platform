0e4fe18ee3ff9eaa07275d1a0e48eef3e637e4a8
Yordan Yordanov <yoyordanov94@gmail.com>
2024-11-25 17:59:06 +0200

    small touch on session toast .. remove default success variant

--- resources/views/livewire/session-toast.blade.php
+++ resources/views/livewire/session-toast.blade.php
@@ -3,21 +3,21 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public function mount()
+    public function mount(): void
     {
         if (session()->has('toast')) {
             $toast = session('toast');
             $this->showToast(
                 $toast['text'],
                 $toast['heading'] ?? null,
-                $toast['variant'] ?? 'success'
+                $toast['variant'] ?? null
             );
         }
     }
 
     protected $listeners = ['showToast' => 'showToast'];
 
-    public function showToast($text, $heading = null, $variant = 'success')
+    public function showToast($text, $heading = null, $variant = null): void
     {
         Flux::toast(
             text: $text,

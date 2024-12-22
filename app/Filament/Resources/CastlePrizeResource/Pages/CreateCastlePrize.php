0cc2844841ad9e855c03c9c5a2aeccc336b3b7c4
Yordan Yordanov <yoyordanov94@gmail.com>
2024-12-22 23:09:25 +0200

    wip castlep prizes

--- app/Filament/Resources/CastlePrizeResource/Pages/CreateCastlePrize.php
+++ app/Filament/Resources/CastlePrizeResource/Pages/CreateCastlePrize.php
@@ -13,25 +13,17 @@ class CreateCastlePrize extends CreateRecord
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $startDate = Carbon::parse($data['period_starts_at'])
-            ->startOfWeek();  // Ensure it's Monday
+            ->startOfWeek();
 
         $data['period_starts_at'] = $startDate;
 
         $data['period_ends_at'] = $startDate
             ->copy()
             ->addWeeks((int) $data['distribution_weeks'])
-            ->previous('Sunday')  // Get to the last Sunday
+            ->previous('Sunday')
             ->setTime(23, 59, 59);
 
         $data['remaining_prize_pool'] = $data['total_prize_pool'];
-
-        return $data;
-    }
-
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        $data['period_starts_at'] = Carbon::parse($data['period_starts_at'])->startOfWeek();
-        $data['period_ends_at'] = Carbon::parse($data['period_ends_at'])->endOfWeek();
 
         return $data;
     }

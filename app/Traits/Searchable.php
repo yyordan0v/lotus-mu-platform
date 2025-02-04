f1b1afc9bf93a6d587bb86f07b239dc1a1cd6ba7
Yordan Yordanov <yoyordanov94@gmail.com>
2025-02-04 20:53:13 +0200

    improve search

--- app/Traits/Searchable.php
+++ app/Traits/Searchable.php
@@ -8,16 +8,23 @@ trait Searchable
 
     protected function searchCharacter($query)
     {
-        return $this->search === ''
-            ? $query
-            : $query->where('Name', 'like', '%'.$this->search.'%');
+        if ($this->search === '') {
+            return $query;
+        }
+
+        return $query->where(function ($query) {
+            $query->where('Name', 'like', $this->search.'%')
+                ->orWhereHas('guildMember.guild', function ($query) {
+                    $query->where('G_Name', 'like', $this->search.'%');
+                });
+        });
     }
 
     protected function searchGuild($query)
     {
         return $this->search === ''
             ? $query
-            : $query->where('G_Name', 'like', '%'.$this->search.'%');
+            : $query->where('G_Name', 'like', $this->search.'%');
     }
 
     public function updatedSearchable($property): void

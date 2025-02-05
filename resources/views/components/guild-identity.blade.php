0583021b678a7eba2ac18b02c73cd38d005c9f4c
Yordan Yordanov <yoyordanov94@gmail.com>
2025-02-05 13:42:28 +0200

    update guild identity component

--- resources/views/components/guild-identity.blade.php
+++ resources/views/components/guild-identity.blade.php
@@ -1,22 +1,17 @@
 @props([
-    'guild' => null,
     'guildMember' => null,
     'href' => '#'
 ])
 
-@php
-    $guildInstance = $guild ?? ($guildMember?->guild()->first());
-@endphp
-
 <div class="flex items-center w-full gap-2">
-    @if($guildInstance)
+    @if($guildMember?->guild)
         <flux:link variant="ghost" :$href wire:navigate.hover class="flex items-center gap-x-2">
-            <img src="{{ $guildInstance->getMarkUrl(24) }}"
+            <img src="{{ $guildMember->guild->getMarkUrl(24) }}"
                  alt="Guild Mark"
                  class="w-6 h-6 shrink-0 rounded border border-zinc-200 dark:border-white/10"
             />
             <span class="max-sm:hidden">
-                {{ $guildInstance->G_Name }}
+                {{ $guildMember->guild->G_Name }}
             </span>
         </flux:link>
     @else

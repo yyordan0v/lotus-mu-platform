3581a95b64ead558c9765396f2bc4acd4afa2cee
Yordan Yordanov <yoyordanov94@gmail.com>
2025-02-07 23:33:10 +0200

    wip

--- resources/views/livewire/pages/guest/rankings/players/archive.blade.php
+++ resources/views/livewire/pages/guest/rankings/players/archive.blade.php
@@ -60,101 +60,56 @@ new #[Layout('layouts.guest')] class extends Component {
                 <flux:tab name="{{ RankingScoreType::HUNTERS->value }}">Hunt Archive</flux:tab>
             </flux:tabs>
 
-            <flux:tab.panel name="{{ RankingScoreType::EVENTS->value }}">
-                <flux:accordion transition>
-                    @forelse($this->periods as $period => $rankings)
-                        <flux:accordion.item>
-                            <flux:accordion.heading>
-                                <div class="flex items-center gap-2">
-                                    <flux:icon.calendar-date-range variant="mini"/>
-                                    <span>{{ $this->formatPeriodDate($period) }}</span>
-                                </div>
-                            </flux:accordion.heading>
+            @foreach(RankingScoreType::cases() as $scoreType)
+                <flux:tab.panel name="{{ $scoreType }}">
+                    <flux:accordion transition>
+                        @forelse($this->periods as $period => $rankings)
+                            <flux:accordion.item>
+                                <flux:accordion.heading>
+                                    <div class="flex items-center gap-2">
+                                        <flux:icon.calendar-date-range variant="mini"/>
+                                        <span>{{ $this->formatPeriodDate($period) }}</span>
+                                    </div>
+                                </flux:accordion.heading>
 
-                            <flux:accordion.content>
-                                <flux:table>
-                                    <flux:columns>
-                                        <flux:column>#</flux:column>
-                                        <flux:column>{{ __('Character') }}</flux:column>
-                                        <flux:column>{{ __('Score') }}</flux:column>
-                                        <flux:column>{{ __('Reward') }}</flux:column>
-                                    </flux:columns>
+                                <flux:accordion.content>
+                                    <flux:table>
+                                        <flux:columns>
+                                            <flux:column>#</flux:column>
+                                            <flux:column>{{ __('Character') }}</flux:column>
+                                            <flux:column>{{ __('Score') }}</flux:column>
+                                            <flux:column>{{ __('Reward') }}</flux:column>
+                                        </flux:columns>
 
-                                    <flux:rows>
-                                        @foreach($rankings as $ranking)
-                                            <flux:row>
-                                                <flux:cell>{{ $ranking->rank }}.</flux:cell>
-                                                <flux:cell>{{ $ranking->character_name }}</flux:cell>
-                                                <flux:cell>{{ number_format($ranking->score) }}</flux:cell>
-                                                <flux:cell class="space-x-1">
-                                                    @foreach($ranking->rewards_given as $reward)
-                                                        <x-resource-badge
-                                                            :value="$reward['amount']"
-                                                            :resource="ResourceType::from($reward['type'])"
-                                                        />
-                                                    @endforeach
-                                                </flux:cell>
-                                            </flux:row>
-                                        @endforeach
-                                    </flux:rows>
-                                </flux:table>
-                            </flux:accordion.content>
-                        </flux:accordion.item>
-                    @empty
-                        <div class="text-center py-4">
-                            {{ __('No archived rankings found.') }}
-                        </div>
-                    @endforelse
-                </flux:accordion>
-            </flux:tab.panel>
+                                        <flux:rows>
+                                            @foreach($rankings as $ranking)
+                                                <flux:row>
+                                                    <flux:cell>{{ $ranking->rank }}.</flux:cell>
+                                                    <flux:cell>{{ $ranking->character_name }}</flux:cell>
+                                                    <flux:cell>{{ number_format($ranking->score) }}</flux:cell>
+                                                    <flux:cell class="space-x-1">
+                                                        @foreach($ranking->rewards_given as $reward)
+                                                            <x-resource-badge
+                                                                :value="$reward['amount']"
+                                                                :resource="ResourceType::from($reward['type'])"
+                                                            />
+                                                        @endforeach
+                                                    </flux:cell>
+                                                </flux:row>
+                                            @endforeach
+                                        </flux:rows>
+                                    </flux:table>
+                                </flux:accordion.content>
+                            </flux:accordion.item>
+                        @empty
+                            <div class="text-center py-4">
+                                {{ __('No archived rankings found.') }}
+                            </div>
+                        @endforelse
+                    </flux:accordion>
+                </flux:tab.panel>
+            @endforeach
 
-            <flux:tab.panel name="{{ RankingScoreType::HUNTERS->value }}">
-                <flux:accordion transition>
-                    @forelse($this->periods as $period => $rankings)
-                        <flux:accordion.item>
-                            <flux:accordion.heading>
-                                <div class="flex items-center gap-2">
-                                    <flux:icon.calendar-date-range variant="mini"/>
-                                    <span>{{ $this->formatPeriodDate($period) }}</span>
-                                </div>
-                            </flux:accordion.heading>
-
-                            <flux:accordion.content>
-                                <flux:table>
-                                    <flux:columns>
-                                        <flux:column>#</flux:column>
-                                        <flux:column>{{ __('Character') }}</flux:column>
-                                        <flux:column>{{ __('Score') }}</flux:column>
-                                        <flux:column>{{ __('Reward') }}</flux:column>
-                                    </flux:columns>
-
-                                    <flux:rows>
-                                        @foreach($rankings as $ranking)
-                                            <flux:row>
-                                                <flux:cell>{{ $ranking->rank }}.</flux:cell>
-                                                <flux:cell>{{ $ranking->character_name }}</flux:cell>
-                                                <flux:cell>{{ number_format($ranking->score) }}</flux:cell>
-                                                <flux:cell class="space-x-1">
-                                                    @foreach($ranking->rewards_given as $reward)
-                                                        <x-resource-badge
-                                                            :value="$reward['amount']"
-                                                            :resource="ResourceType::from($reward['type'])"
-                                                        />
-                                                    @endforeach
-                                                </flux:cell>
-                                            </flux:row>
-                                        @endforeach
-                                    </flux:rows>
-                                </flux:table>
-                            </flux:accordion.content>
-                        </flux:accordion.item>
-                    @empty
-                        <div class="text-center py-4">
-                            {{ __('No archived rankings found.') }}
-                        </div>
-                    @endforelse
-                </flux:accordion>
-            </flux:tab.panel>
         </flux:tab.group>
     </flux:card>
 </flux:main>

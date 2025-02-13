daa01cb192de9614c3249c4dc648bb572223c7dc
Yordan Yordanov <yoyordanov94@gmail.com>
2025-02-13 21:06:51 +0200

    adjust guild master cell

--- resources/views/components/rankings/table/cells/guild-master.blade.php
+++ resources/views/components/rankings/table/cells/guild-master.blade.php
@@ -1,6 +1,5 @@
 @props([
     'character',
-    'href' => '#',
 ])
 
 @php
@@ -8,13 +7,16 @@
 @endphp
 
 <flux:cell>
-    <flux:link variant="ghost" :$href wire:navigate.hover class="flex items-center space-x-2">
+    <flux:link variant="ghost"
+               :href="route('character', ['name' => $character->Name])"
+               wire:navigate.hover
+               class="flex items-center space-x-2">
         <flux:avatar size="xs" src="{{ asset($character->Class->getImagePath()) }}"/>
 
         <span>{{ $character->Name }}</span>
 
-        @if($character->member->AccountLevel !== AccountLevel::Regular)
-            <flux:icon.fire variant="mini" class="text-{{ $character->member->AccountLevel->badgeColor() }}-500"/>
+        @if($character?->member?->AccountLevel !== AccountLevel::Regular)
+            <flux:icon.fire variant="mini" class="text-{{ $character?->member?->AccountLevel->badgeColor() }}-500"/>
         @endif
     </flux:link>
 </flux:cell>

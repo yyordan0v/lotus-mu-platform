2c3fa7233fbcf0be85f790497a425c2ed1a793f4
Yordan Yordanov <yoyordanov94@gmail.com>
2025-02-23 15:25:02 +0200

    videos section texts

--- resources/views/components/home/feature-split.blade.php
+++ resources/views/components/home/feature-split.blade.php
@@ -5,8 +5,8 @@
 ])
 
 @if($type == 'video')
-    <div class="lg:grid lg:grid-cols-12 lg:gap-8 pt-4 xl:pt-0 mt-4 xl:mt-0">
-        <div class="lg:col-span-4 lg:mt-10 xl:mt-18">
+    <div class="lg:grid lg:grid-cols-12 lg:gap-8 pt-4 xl:pt-0 mt-4 xl:mt-0 max-lg:space-y-8">
+        <div class="lg:col-span-4 lg:mt-10 xl:mt-18 space-y-4">
             {{ $text }}
         </div>
 
--- resources/views/components/home/sections/more-features.blade.php
+++ resources/views/components/home/sections/more-features.blade.php
@@ -41,7 +41,7 @@
                 </flux:tab>
 
                 <flux:tab name="events" :accent="false" icon="award">
-                    {{ __('Elite Events') }}
+                    {{ __('Events') }}
                 </flux:tab>
 
                 <flux:tab name="pets" :accent="false" icon="paw-print">
@@ -49,14 +49,36 @@
                 </flux:tab>
             </flux:tabs>
 
-            <div class="absolute w-full overflow-visible left-0 h-[30rem] mt-8 xl:mt-16">
+            <div class="absolute w-full overflow-visible left-0 h-[30rem] mt-8 xl:mt-16 -z-10">
                 <x-home.grid-pattern position="top_left"/>
             </div>
 
             <flux:tab.panel name="wiki">
                 <x-home.feature-split type="video">
                     <x-slot:text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+                        <flux:heading size="lg">
+                            {{ __('Wiki') }}
+                        </flux:heading>
+
+                        <div class="space-y-2">
+                            <flux:text>
+                                {{  __('Ladies and gentlemen - the most comprehensive MU knowledge base in the world -') }}
+
+                                <flux:link variant="ghost"
+                                           href="https://wiki.lotusmu.org"
+                                           external>
+                                    {{ __('the Lotus Mu Wiki.') }}
+                                </flux:link>
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('What truly sets Lotus Mu apart is our dedication to knowledge sharing and documentation.') }}
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('Find everything you need in seconds through our modern interface and powerful search engine.') }}
+                            </flux:text>
+                        </div>
                     </x-slot:text>
 
                     <x-slot:media>
@@ -71,7 +93,31 @@
             <flux:tab.panel name="items">
                 <x-home.feature-split type="video">
                     <x-slot:text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit...
+                        <flux:heading size="lg">
+                            {{ __('Evolving Items') }}
+                        </flux:heading>
+
+                        <div class="space-y-2">
+                            <flux:text>
+                                {{  __('Think Venom Mist should be more powerful than the Pad set?') }}
+                            </flux:text>
+
+                            <flux:text class="font-bold">
+                                {{ __('We think so too!') }}
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('In modern MU Online, having the same excellent options across all items doesn\'t make sense - that\'s why our excellent options scale with item tiers - stronger items, better bonuses.') }}
+                            </flux:text>
+
+                            <flux:text>{{ _('Want to see how it works?') }}
+                                <flux:link variant="ghost"
+                                           href="https://wiki.lotusmu.org/items/exc-items"
+                                           external>
+                                    {{ __('Check out the details!') }}
+                                </flux:link>
+                            </flux:text>
+                        </div>
                     </x-slot:text>
 
                     <x-slot:media>
@@ -86,8 +132,31 @@
             <flux:tab.panel name="quests">
                 <x-home.feature-split type="video">
                     <x-slot:text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit... Lorem ipsum dolor sit amet,
-                        consectetur adipisicing elit. Ipsam, minima?
+                        <flux:heading size="lg">
+                            {{ __('Progressive Quests') }}
+                        </flux:heading>
+
+                        <div class="space-y-2">
+                            <flux:text>
+                                {{  __('Ever wondered if quests could actually help you progress?') }}
+                            </flux:text>
+
+                            <flux:text class="font-bold">
+                                {{ __('In Lotus MU, they absolutely do!') }}
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('From hunting spiders to challenging mighty dragons, our quest system grows with you. Each completed quest rewards you with valuable items and points - making your character stronger every step of the way.') }}
+                            </flux:text>
+
+                            <flux:text>{{ _('Ready to start your journey?') }}
+                                <flux:link variant="ghost"
+                                           href="https://wiki.lotusmu.org/gameplay-systems/quest-system"
+                                           external>
+                                    {{ __('See the full quest line!') }}
+                                </flux:link>
+                            </flux:text>
+                        </div>
                     </x-slot:text>
 
                     <x-slot:media>
@@ -102,9 +171,31 @@
             <flux:tab.panel name="crafting">
                 <x-home.feature-split type="video">
                     <x-slot:text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit... Lorem ipsum dolor sit amet,
-                        consectetur adipisicing elit. Ipsam, minima? Lorem ipsum dolor sit amet, consectetur
-                        adipisicing elit. Consectetur, minus.
+                        <flux:heading size="lg">
+                            {{ __('Crafting System') }}
+                        </flux:heading>
+
+                        <div class="space-y-2">
+                            <flux:text>
+                                {{  __('Think crafting in MU Online needs a fresh take?') }}
+                            </flux:text>
+
+                            <flux:text class="font-bold">
+                                {{ __('We redesigned it completely!') }}
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('From unique pet crafting to reimagined classic recipes, our system aligns perfectly with modern MU gameplay - multiple resets, active farming, and exciting rewards.') }}
+                            </flux:text>
+
+                            <flux:text>{{ _('Curious about crafting?') }}
+                                <flux:link variant="ghost"
+                                           href="https://wiki.lotusmu.org/category/crafting"
+                                           external>
+                                    {{ __('See what\'s cooking!') }}
+                                </flux:link>
+                            </flux:text>
+                        </div>
                     </x-slot:text>
 
                     <x-slot:media>
@@ -119,9 +210,33 @@
             <flux:tab.panel name="events">
                 <x-home.feature-split type="video">
                     <x-slot:text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit... Lorem ipsum dolor sit amet,
-                        consectetur adipisicing elit. Ipsam, minima? Lorem ipsum dolor sit amet, consectetur
-                        adipisicing elit. Consectetur, minus.
+                        <flux:heading size="lg">
+                            {{ __('Events') }}
+                        </flux:heading>
+
+                        <div class="space-y-2">
+                            <flux:text>
+                                {{  __('From intense 1v1 battles in the Hall of Fame to team clashes and PvE challenges - our events are designed for every playstyle.') }}
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('Top performers in our weekly rankings receive special rewards for their dedication.') }}
+                            </flux:text>
+
+                            <flux:text>{{ _('Want the full schedule?') }}
+                                <flux:link variant="ghost"
+                                           :href="route('schedule')"
+                                           wire:navigate.hover>
+                                    {{ __('Check event times') }}
+                                </flux:link>
+                                {{ __('or') }}
+                                <flux:link variant="ghost"
+                                           href="https://wiki.lotusmu.org/category/events"
+                                           external>
+                                    {{ __('read our guides.') }}
+                                </flux:link>
+                            </flux:text>
+                        </div>
                     </x-slot:text>
 
                     <x-slot:media>
@@ -136,9 +251,31 @@
             <flux:tab.panel name="pets">
                 <x-home.feature-split type="video">
                     <x-slot:text>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit... Lorem ipsum dolor sit amet,
-                        consectetur adipisicing elit. Ipsam, minima? Lorem ipsum dolor sit amet, consectetur
-                        adipisicing elit. Consectetur, minus.
+                        <flux:heading size="lg">
+                            {{ __('Pet Collection') }}
+                        </flux:heading>
+
+                        <div class="space-y-2">
+                            <flux:text>
+                                {{  __('Looking for a powerful companion?') }}
+                            </flux:text>
+
+                            <flux:text class="font-bold">
+                                {{ __('Our pets are game-changers!') }}
+                            </flux:text>
+
+                            <flux:text>
+                                {{ __('From the mighty Warhorse to the fearsome Ice Dragon, each class has its own unique set of companions. With three power tiers and class-specific bonuses, these pets aren\'t just for show.') }}
+                            </flux:text>
+
+                            <flux:text>{{ _('Curious about their powers?') }}
+                                <flux:link variant="ghost"
+                                           href="https://wiki.lotusmu.org/items/pets"
+                                           external>
+                                    {{ __('Meet the pets!') }}
+                                </flux:link>
+                            </flux:text>
+                        </div>
                     </x-slot:text>
 
                     <x-slot:media>

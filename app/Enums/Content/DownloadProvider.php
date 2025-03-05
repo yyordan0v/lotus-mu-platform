54624e8e4759cddacda770d8dc5b02ec21bab43a
Yordan Yordanov <yoyordanov94@gmail.com>
2025-03-05 21:58:49 +0200

    add yandex disk icon

--- app/Enums/Content/DownloadProvider.php
+++ app/Enums/Content/DownloadProvider.php
@@ -7,6 +7,7 @@ enum DownloadProvider: string
     case GOOGLE_DRIVE = 'google_drive';
     case MEGA = 'mega';
     case MEDIAFIRE = 'mediafire';
+    case YANDEX = 'yandex';
     case DEFAULT = 'default';
 
     public function getIcon(): ?string
@@ -15,6 +16,7 @@ enum DownloadProvider: string
             self::GOOGLE_DRIVE => 'icons.providers.google-drive',
             self::MEGA => 'icons.providers.mega',
             self::MEDIAFIRE => 'icons.providers.mediafire',
+            self::YANDEX => 'icons.providers.yandex-disk',
             self::DEFAULT => 'heroicon-o-cloud-arrow-down',
         };
     }
@@ -25,6 +27,7 @@ enum DownloadProvider: string
             str_contains($url, 'drive.google.com') => self::GOOGLE_DRIVE,
             str_contains($url, 'mega.nz') => self::MEGA,
             str_contains($url, 'mediafire.com') => self::MEDIAFIRE,
+            str_contains($url, 'disk.yandex.com') => self::YANDEX,
             default => self::DEFAULT
         };
     }
--- resources/views/livewire/pages/guest/files/index.blade.php
+++ resources/views/livewire/pages/guest/files/index.blade.php
@@ -38,11 +38,11 @@ new #[Layout('layouts.guest')] class extends Component {
         </x-info-card>
 
         @if($this->downloads()->count() > 0)
-            <div class="grid grid-cols-3 max-md:grid-cols-1 gap-6">
+            <div class="grid grid-cols-4 max-lg:grid-cols-2 max-md:grid-cols-1 gap-6">
                 @foreach($this->downloads() as $download)
                     <a href="{{ $download['url'] }}" target="_blank">
                         <flux:card
-                            class="flex flex-1 items-center gap-4 w-full hover:bg-zinc-50 dark:hover:bg-white/5 transition-colors group">
+                            class="flex flex-1 items-center gap-4 w-full hover:bg-zinc-100 dark:hover:bg-white/5 transition-colors group">
                             <x-dynamic-component
                                 :component="$download['icon']"
                                 class="w-8 h-8"

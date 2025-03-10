4c17062927bb250eaf93fdb9e1cdb84b4a038821
Yordan Yordanov <yoyordanov94@gmail.com>
2025-03-10 22:44:29 +0200

    remove strict models from production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Model::shouldBeStrict();
+        $this->app->isProduction() || Model::shouldBeStrict();
 
         Vite::prefetch(3);
     }

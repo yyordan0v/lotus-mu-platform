66597a32081bf5546724b509ebc48ebe102b9ac7
Yordan Yordanov <yoyordanov94@gmail.com>
2025-03-14 15:34:35 +0200

    change date format in event schedule

--- resources/views/livewire/pages/guest/schedule/item.blade.php
+++ resources/views/livewire/pages/guest/schedule/item.blade.php
@@ -63,9 +63,13 @@ new class extends Component {
         },
 
         formatDateTime(date) {
-            return date.toLocaleString('en-US', {
-                year: 'numeric', month: '2-digit', day: '2-digit',
-                hour: '2-digit', minute: '2-digit', hour12: false
+            return date.toLocaleString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: false
             });
         },
 

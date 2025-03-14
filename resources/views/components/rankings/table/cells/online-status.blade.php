d627ad8f77470d83cf46f8f9657873152ddedfca
Yordan Yordanov <45233833+yyordan0v@users.noreply.github.com>
2025-03-14 19:25:58 +0200

    Update online-status.blade.php

--- resources/views/components/rankings/table/cells/online-status.blade.php
+++ resources/views/components/rankings/table/cells/online-status.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $classes = Flux::classes()
-        ->add('w-1.5 h-1.5 rounded-full')
+        ->add('w-1.5 h-1.5 rounded-full pulse')
         ->add(match ($status) {
             true => 'bg-emerald-400/80',
             false => 'bg-rose-400/80',
